4afdb133944c99ac7734ba7c7b36545fcd3b9872
Joshua McDowell <josh@Joshuas-MBP.lan1>
2022-05-13 16:18:45 -0500

    added type ahead to index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 
 import Input from "./Input";
+import TypeAheadDropdown from "./TypeAheadDropdown";
 import useFormHandlers from "./useFormHandlers";
 
 export {
     Input,
+    TypeAheadDropdown,
     useFormHandlers,
 };
